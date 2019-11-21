183d4d0e27e1834ff8a079883f69834791c70744
vikas-ambab <vikas.gupta@ambab.com>
2019-11-21 21:52:09 +0530

    login working

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -14,29 +14,52 @@ import { login } from "../../request/auth";
 import serialize from "form-serialize";
 import notify from "../../utils/notify";
 import getErrorMessage from "../../utils/getErrorMessage";
+import { withFormik } from "formik";
+import * as yup from "yup";
+
+const loginSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .email("Username must be a valid username.")
+    .required("Username is a required field."),
+  password: yup
+    .string()
+    .trim()
+    .required("password is a required field.")
+});
+
+const submitHandler = async values => {
+  console.log(values);
+  // const form = e.target;
+  // const formdata = serialize(form, { hash: true });
+  try {
+    const user = await login(values);
+    // console.log(user.access_token);
+    initAxios();
+    localStorage.setItem("userDetail", JSON.stringify(user));
+    // this.props.history.replace("/users");
+  } catch (error) {
+    if (error.response.status === 401) {
+      notify({
+        type: "error",
+        text: getErrorMessage(error.response.data)
+      });
+    }
+  }
+};
 
 class Login extends Component {
-  submitHandler = async e => {
-    e.preventDefault();
-    const form = e.target;
-    const formdata = serialize(form, { hash: true });
-    try {
-      const user = await login(formdata);
-      // console.log(user.access_token);
-      initAxios();
-      localStorage.setItem("userDetail", JSON.stringify(user));
-      // this.props.history.replace("/users");
-    } catch (error) {
-      if (error.response.status === 401) {
-        notify({
-          type: "error",
-          text: getErrorMessage(error.response.data)
-        });
-      }
-    }
-  };
-
   render() {
+    const {
+      handleSubmit,
+      errors,
+      touched,
+      handleBlur,
+      isSubmitting,
+      setFieldValue,
+      handleChange
+    } = this.props;
     return (
       <Container className="mt-5">
         <Row>
@@ -45,7 +68,7 @@ class Login extends Component {
               <img src="/images/logo.png" alt="demo-logo" width="70px" />{" "}
             </div>
             <div className="card-body">
-              <Form onSubmit={this.submitHandler}>
+              <Form onSubmit={handleSubmit}>
                 <FormGroup>
                   <div className="form-label-group">
                     <Input
@@ -53,8 +76,15 @@ class Login extends Component {
                       name="username"
                       placeholder="Username"
                       autoComplete="off"
+                      onChange={handleChange}
+                      invalid={Boolean(touched.username && errors.username)}
+                      valid={!!(touched.username && !errors.username)}
+                      onBlur={handleBlur}
                     />
                   </div>
+                  <FormFeedback style={{ display: "block" }}>
+                    {errors.username}
+                  </FormFeedback>
                 </FormGroup>
                 <FormGroup>
                   <div className="form-label-group">
@@ -63,8 +93,15 @@ class Login extends Component {
                       name="password"
                       placeholder="********"
                       autoComplete="off"
+                      onChange={handleChange}
+                      invalid={Boolean(touched.password && errors.password)}
+                      valid={!!(touched.password && !errors.password)}
+                      onBlur={handleBlur}
                     />
                   </div>
+                  <FormFeedback style={{ display: "block" }}>
+                    {errors.password}
+                  </FormFeedback>
                 </FormGroup>
                 <FormGroup>
                   <Button size="sm" type="submit" color="primary" block>
@@ -80,4 +117,12 @@ class Login extends Component {
   }
 }
 
-export default Login;
+// export default Login;
+export default withFormik({
+  mapPropsToValues: props => ({
+    username: props.username,
+    password: props.password
+  }),
+  validationSchema: loginSchema,
+  handleSubmit: submitHandler
+})(Login);
